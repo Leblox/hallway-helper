cf289587f49b160d83013d9eb4831ef1fef3ed3c
Lena <l024@tutanota.com>
2020-11-12 16:02:32 +0000

    Updated 7timer to include proxy for https

--- app/assets/scripts/models/Weather.js
+++ app/assets/scripts/models/Weather.js
@@ -7,7 +7,10 @@ export default class Weather {
 
     async getWeather(latitude, longitude) {
         try {
-           let result = await axios(`http://www.7timer.info/bin/api.pl?lon=${longitude}&lat=${latitude}&product=civillight&output=json`);
+            let proxy = 'https://cors-anywhere.herokuapp.com/';
+
+            // 7timer needs proxy due to security issues (http and not https) leading to blocked mixed media issues on deployment
+            let result = await axios(`${proxy}http://www.7timer.info/bin/api.pl?lon=${longitude}&lat=${latitude}&product=civillight&output=json`);
 
             this.forecast = result.data.dataseries[0];
 
